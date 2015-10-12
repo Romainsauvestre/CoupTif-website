e6411e6a4f81b5720d1ea28122ba5499d77ab1d4
olivier.moindrot <omoindrot@gmail.com>
2015-10-13 00:05:49 +0200

    Probleme dans la creation du modele services, a refaire proprement

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,16 +1,75 @@
 class ServicesController < ApplicationController
   before_action :set_service, only: [:show, :edit, :update, :destroy]
 
-  # GET /hairdressers/:id/services
-  # GET /hairdressers/:id/services.json
+  # GET /hairdressers/:hairdresser_id/services
+  # GET /hairdressers/:hairdresser_id/services.json
   def index
-    @services = Service.find_by_hairdresser_id(@id)
+    @services = Service.find_by_hairdresser_id(@hairdresser_id)
   end
 
-  # GET /hairdressers/:id/services/new
+  # GET /hairdressers/:hairdresser_id/services/:id
+  # GET /hairdressers/:hairdresser_id/services/:id.json
+  def show
+  end
 
+  # GET /hairdressers/:hairdresser_id/services/new
   def new
-    @services = Service.new
+    @service = Service.new
+  end
+
+  # GET /hairdressers/:hairdresser_id/services/:id/edit
+  def edit
+  end
+
+  # POST /hairdressers/:hairdresser_id/services
+  # POST /hairdressers/:hairdresser_id/services.json
+  def create
+    @service = Service.new(service_params)
+
+    respond_to do |format|
+      if @service.save
+        format.html { redirect_to @service, notice: 'Service was successfully created.' }
+        format.json { render :show, status: :created, location: @service }
+      else
+        format.html { render :new }
+        format.json { render json: @service.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /hairdressers/:hairdresser_id/services/:id
+  # PATCH/PUT /hairdressers/:hairdresser_id/services/:id.json
+  def update
+    respond_to do |format|
+      if @service.update(service_params)
+        format.html { redirect_to @service, notice: 'Service was successfully updated.' }
+        format.json { render :show, status: :ok, location: @service }
+      else
+        format.html { render :edit }
+        format.json { render json: @service.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /hairdressers/:hairdresser_id/services/:id
+  # DELETE /hairdressers/:hairdresser_id/services/:id.json
+  def destroy
+    @service.destroy
+    respond_to do |format|
+      format.html { redirect_to services_url, notice: 'Service was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def service_params
+    params.require(:service).permit(:name, :price, :description, :photo, @hairdresser_id)
   end
 
 end
