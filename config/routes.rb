04acbdba5492d68a1d21400ada6f27aba1e5afb7
olivier.moindrot <omoindrot@gmail.com>
2015-10-20 17:03:54 +0200

    Added user class with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :hairdressers do
     resources :services
   end
 
+  root 'hairdressers#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
